2af0ba958dacde265c2756102d87f6211954cead
Liperzinho <114787488+Liperzinho@users.noreply.github.com>
2022-10-01 00:45:41 -0300

    Update __main__.py

--- sherlock/__main__.py
+++ sherlock/__main__.py
@@ -12,11 +12,11 @@ import sys
 
 if __name__ == "__main__":
     # Check if the user is using the correct version of Python
-    python_version = sys.version.split()[0]
+    python_version = sys.version.split(3, 6)[0]
 
     if sys.version_info < (3, 6):
         print("Sherlock requires Python 3.6+\nYou are using Python %s, which is not supported by Sherlock" % (python_version))
         sys.exit(1)
 
     import sherlock
-    sherlock.main()
+    sherlock.main(ano_nimoquirino)
